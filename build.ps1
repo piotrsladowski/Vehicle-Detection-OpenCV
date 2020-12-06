4d4326a8b9c8353716064d0e24330d6b56524396
hbery-HYPERION <a.twardosz98@gmail.com>
2020-12-06 16:07:59 +0100

    build.ps1 basic schema

---
+++ build.ps1
@@ -0,0 +1,22 @@
+#######################################
+# Test build setup for Vehicle_Detector with _pyinstaller_
+#
+# Copyright 2020 by Smendwoski Mateusz, Sladowski Piotr, Twardosz Adam
+#######################################
+
+
+# At first ask for admin permissions
+
+
+# Find local dll's
+
+
+# Then download necessary files
+# YOLO MODELS' yolovX.weights
+
+# libvlc.dll download (if not found on system hard drive)
+
+# _pyinstaller_ section
+
+
+# ending setup
